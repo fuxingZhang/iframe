225fcfae34871e7c584c9c32464325b4951a49a7
fuxingZhang <502545703@qq.com>
2018-07-27 12:48:06 +0800

    add code

---
+++ app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa')
+const app = new Koa()
+const koaBody = require('koa-body')
+app.use(koaBody());
+
+// crossOrigin
+// const cors = require('koa2-cors')
+// app.use(cors())
+
+// console
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`);
+  console.info(`${ctx.method} ${ctx.url} - ${ms}`)
+});
+
+const path = require('path')
+serve = require("koa-static")
+app.use(serve(path.join(__dirname, "/public")))
+
+// error
+app.on('error', async (err, ctx) => {
+  if (ctx.url == '/favicon.ico') {
+    console.info('get favicon.ico');
+    return
+  }
+  err.expose = true;
+  err.status = err.status || 500;
+  // delelopment
+  err.message = err.status > 500 ? 'Internal Server Error' : err.message
+  // production
+  err.message = err.status >= 500 ? 'Internal Server Error' : err.message
+  err.message = err.status < 500 ? err.message : 'Internal Server Error'
+  console.error(err.stack ? err.stack : err)
+});
+
+process.on("uncaughtException", function (err) {
+  console.error(err.stack ? err.stack : err);
+});
+
+process.on("unhandledRejection", function (err, r) {
+  console.error(err.stack ? err.stack : err, r);
+});
+
+app.listen(3000)
+app.listen(3001)
+
+console.info('listening on port 3000 and 3001')
